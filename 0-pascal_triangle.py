32d1391b63d3c23a5be57fcafac1aee9cdc34621
elassari19 <elassari19@gmail.com>
2024-03-07 01:00:50 +0100

    excutable file

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,16 +3,16 @@
 
 
 def pascal_triangle(n):
-    ''' returns a list of integers representing the pascal triangle'''
-    p = []
-    if n <= 0:
-        return p
-    p = [[1]]
-    for i in range(1, n):
-        temp = [1]
-        for j in range(len(p[i - 1]) - 1):
-            curr = p[i - 1]
-            temp.append(p[i - 1][j] + p[i - 1][j + 1])
-        temp.append(1)
-        p.append(temp)
-    return p
+	''' returns a list of integers representing the pascal triangle'''
+	k = []
+	if n <= 0:
+		return k
+	k = [[1]]
+	for i in range(1, n):
+		temp = [1]
+		for j in range(len(k[i - 1]) - 1):
+			curr = k[i - 1]
+			temp.append(k[i - 1][j] + k[i - 1][j + 1])
+		temp.append(1)
+		k.append(temp)
+	return k
