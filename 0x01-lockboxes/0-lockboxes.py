a5f80676ef56a5c797188291f6d798491c77183c
elassari19 <elassari19@gmail.com>
2024-03-17 02:38:13 +0000

    0x01-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,17 +5,17 @@ should write a function that determines if all the boxes can be opened
 
 
 def canUnlockAll(boxes):
-		""" determines boxes """
-		if type(boxes) is not list:
-				return False
-		elif (len(boxes)) == 0:
-				return False
-		for k in range(1, len(boxes) - 1):
-				boxes_checked = False
-				for idx in range(len(boxes)):
-						boxes_checked = k in boxes[idx] and k != idx
-						if boxes_checked:
-								break
-				if boxes_checked is False:
-						return boxes_checked
-		return True
+    """ determines boxes """
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
