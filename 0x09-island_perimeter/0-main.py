f91dad70cb9f549974bad15f83016cd569183b51
elassari19 <elassari19@gmail.com>
2024-05-06 22:34:16 +0100

    0x09-island_perimeter

---
+++ 0x09-island_perimeter/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" dould def island_perimeter(grid) module """
+island_perimeter = __import__('0-island_perimeter').island_perimeter
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
